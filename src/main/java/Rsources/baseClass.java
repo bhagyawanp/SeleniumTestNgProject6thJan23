355b1cb28b31f4f3ba5f32a05d6ac1715dd58a9b
bhagyawanp <bhagyawanp@gmail.com>
2023-05-12 20:44:54 +0530

    added latest report changes

--- src/main/java/Rsources/baseClass.java
+++ src/main/java/Rsources/baseClass.java
@@ -1,73 +1,98 @@
 package Rsources;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 public class baseClass {
-	
-	public WebDriver driver;
+
+	public static WebDriver driver;
 	public Properties prop;
 
 	public void initializeDriver() throws IOException {
-		
-		//This will access the properties file
-		FileInputStream   fis=new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\Rsources\\data.properties");
-		
-		//Read the file-
-		
-		 prop=new Properties();
-		
+
+		// This will access the properties file
+		FileInputStream fis = new FileInputStream(
+				System.getProperty("user.dir") + "\\src\\main\\java\\Rsources\\data.properties");
+
+		// Read the file-
+
+		prop = new Properties();
+
 		prop.load(fis);
-		
-		String browserName= prop.getProperty("browser");
-		
-		if(browserName.equalsIgnoreCase("chrome")) {
-			driver= new ChromeDriver();
-			
-		}
-		else if(browserName.equalsIgnoreCase("Firefox")) {
-			 driver= new FirefoxDriver();
-			
-		}
-		else if(browserName.equalsIgnoreCase("Edge")) {
-			 driver= new EdgeDriver();
-			
-		}
-		else {
+
+		String browserName = prop.getProperty("browser");
+
+		if (browserName.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+
+		} else if (browserName.equalsIgnoreCase("Firefox")) {
+			driver = new FirefoxDriver();
+
+		} else if (browserName.equalsIgnoreCase("Edge")) {
+			driver = new EdgeDriver();
+
+		} else {
 			System.out.println("please choose valid browser to run your scripts");
 		}
-		
-		
-		
+
 	}
-	
-	
-	
+
 	@BeforeMethod
 	public void launchBrowser() throws IOException {
-		
-		initializeDriver() ;
-		
-		String url=prop.getProperty("url");
+
+		initializeDriver();
+
+		String url = prop.getProperty("url");
 		driver.get(url);
-		
+
 	}
-	
-	
+
 	@AfterMethod
 	public void quitBrowser() throws IOException {
-		
-	   driver.quit();
-		
+
+		driver.quit();
+
+	}
+
+	@BeforeTest
+	public void ExtentReport() {
+		extentManager.setup();
+	}
+
+	@AfterTest
+	public void endReport() {
+		extentManager.endReport();
+	}
+
+	public static String screenShot(WebDriver driver, String filename) {
+		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+		File source = takesScreenshot.getScreenshotAs(OutputType.FILE);
+		String destination = System.getProperty("user.dir") + "\\ScreenShot\\" + filename + "_" + dateName + ".png";
+		File finalDestination = new File(destination);
+		try {
+			FileUtils.copyFile(source, finalDestination);
+		} catch (Exception e) {
+			e.getMessage();
+		}
+		return destination;
 	}
 
 }
